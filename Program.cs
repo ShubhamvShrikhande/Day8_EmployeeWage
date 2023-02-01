cd39822b577f267bfa28605b07357a684395cee4
“ShubhamvShrikhande” <shubhamvshri99@gmail.com>
2023-02-01 12:48:08 +0530

    UC3- Added

--- Program.cs
+++ Program.cs
@@ -9,29 +9,40 @@ namespace EmployeeWage
         int wagePHour;
         int fullDHour;
         int dailyEmpWage;
+        int partTime;
 
-        public Program(int wageHore,int dayHour,int dailyWage)
+        public Program(int wageHore, int dayHour, int dailyWage, int parttime)
         {
-            wagePHour =wageHore;
+            wagePHour = wageHore;
             fullDHour = dayHour;
-            dailyEmpWage = dailyWage;  
+            dailyEmpWage = dailyWage;
+            partTime = parttime;
         }
         public void ceckAttendance()
         {
-            int present = rd.Next(0, 2);
-            if(present == 1)
+            int present = rd.Next(0, 3);
+            if (present == 1)
             {
                 dailyEmpWage = wagePHour * fullDHour;
                 {
                     Console.WriteLine("emp is presnt and is wage is:" + dailyEmpWage);
                 }
+                if (present == 2)
+                {
+                    dailyEmpWage = wagePHour * partTime;
+                    Console.WriteLine("emp is presnt in hafe day and is wage is:" + dailyEmpWage);
+                }
+                else
+                {
+                    Console.WriteLine("emp is absent and is wage is:" + dailyEmpWage);
+                }
             }
         }
         public static void Main(string[] args)
         {
-            Program pg = new Program(20, 8, 0);
+            Program pg = new Program(20, 8, 4 ,0);
             pg.ceckAttendance();
         }
-        
+
     }
 }
