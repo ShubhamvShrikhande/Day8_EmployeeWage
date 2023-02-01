fb3e08b6c330e7b5cc53b0609d6f02b8dfbbc92f
“ShubhamvShrikhande” <shubhamvshri99@gmail.com>
2023-02-01 13:45:19 +0530

    UC5- Added

--- Program.cs
+++ Program.cs
@@ -10,34 +10,45 @@ namespace EmployeeWage
         int fullDHour;
         int dailyEmpWage;
         int partTime;
+        int totalworkday;
 
-        public Program(int wageHore, int dayHour, int dailyWage, int parttime)
+        public Program(int wageHore, int dayHour, int dailyWage, int parttime, int totalwork)
         {
             wagePHour = wageHore;
             fullDHour = dayHour;
             dailyEmpWage = dailyWage;
             partTime = parttime;
-        }
+            totalworkday = totalwork;
+         }
         public void ceckAttendance()
         {
-            switch (rd.Next(0, 3))
+            int totalWage = 0;
+            int day = 1;
+            while (day <= 30 && totalworkday <= 20)
             {
-                case 1: dailyEmpWage = wagePHour * fullDHour;
+                switch (rd.Next(0, 3))
+                {
+                    case 1: dailyEmpWage = wagePHour * fullDHour;
                         Console.WriteLine("emp is presnt  and wage is:" + dailyEmpWage);
-                break;
+                        break;
 
-                case 2: dailyEmpWage = wagePHour * partTime;
+                    case 2: dailyEmpWage = wagePHour * partTime;
                         Console.WriteLine("emp is presnt  and wage is:" + dailyEmpWage);
-                break;
+                        break;
 
-                default:
-                        Console.WriteLine("emp is absent and wage is:"+dailyEmpWage);
-                break;
+                    default:
+                        Console.WriteLine("emp is absent and wage is:" + dailyEmpWage);
+                        break;
+                }
+                totalWage += dailyEmpWage;
+                day++; 
             }
+             
+
         }
         public static void Main(string[] args)
         {
-            Program pg = new Program(20, 8, 4 ,0);
+            Program pg = new Program(20, 8, 4 ,0,0);
             pg.ceckAttendance();
         }
 
